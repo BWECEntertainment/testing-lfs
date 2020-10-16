f6a512d88bfe2e56af680c583cc81997edf589fe
camdyncc <camdyncoblentz@gmail.com>
2020-10-15 23:16:48 -0400

    animations and trigger events
    Finished weapon animations, set up trigger and bool events to handle animation transitions, working on code fro ADS and bullet handeling.

---
+++ MyFirstGame/Assets/Scripts/Weapons Scripts/WeaponHandler.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum WeaponAim
+{
+    NONE,
+    SELF_AIM,
+    AIM
+}
+
+public enum WeaponFireType
+{
+    SINGLE,
+    MULTIPLE
+}
+
+public enum WeaponBulletType
+{
+    BULLET,
+    ARROW,
+    SPEAR,
+    NONE
+
+
+}
+
+
+
+
+
+
+public class WeaponHandler : MonoBehaviour
+{
+
+    private Animator anim;
+
+    public WeaponAim weapon_Aim;
+
+
+    [SerializeField]
+    private GameObject muzzleFlash;
+
+    [SerializeField]
+    private AudioSource shootSound, reload_Sound;
+
+    public WeaponFireType fireType;
+
+    public WeaponBulletType bulletType;
+
+    public GameObject attack_Point;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        anim = GetComponent<Animator>();
+
+        
+    }
+
+    public void ShootAnimation()
+    {
+        anim.SetTrigger(AnimationTags.SHOOT_TRIGGER);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
